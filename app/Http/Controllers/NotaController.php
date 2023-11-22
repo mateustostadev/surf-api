9adc04e102e99c80efaab4f7e8bcb1041edafd49
Mateus Tosta <116604644+Mateusmbarreto@users.noreply.github.com>
2023-11-22 10:59:46 -0300

    Update NotaController.php

--- app/Http/Controllers/NotaController.php
+++ app/Http/Controllers/NotaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Nota;
 use Illuminate\Http\Request;
+use App\Models\Onda;
 
 class NotaController extends Controller
 {
@@ -23,8 +24,14 @@ class NotaController extends Controller
             'notaParcial3' => $request->input('notaParcial3'),
         ]);
 
+        $onda = Onda::findOrFail($request->input('onda'));
+
         return response()->json([
             'message' => 'Notas cadastradas com sucesso.',
+            'onda_id' => $onda->id,
+            'notaParcial1' => $nota->notaParcial1,
+            'notaParcial2' => $nota->notaParcial2,
+            'notaParcial3' => $nota->notaParcial3,
         ], 201);
     }
-}   
+}
